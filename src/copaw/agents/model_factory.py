a48f867eaeefe7fd8a637caea47b1df8f24faadf
DavdGao <gaodawei.gdw@alibaba-inc.com>
2026-03-04 18:39:26 +0800

    fix

--- src/copaw/agents/model_factory.py
+++ src/copaw/agents/model_factory.py
@@ -34,7 +34,7 @@ def _monkey_patch(func):
     """A monkey patch wrapper for agentscope <= 1.0.16dev"""
 
     @wraps(func)
-    async def wrapper(msgs: list[Msg], **kwargs: Any) -> list[dict[str, Any]]:
+    async def wrapper(self, msgs: list[Msg], **kwargs: Any) -> list[dict[str, Any]]:
         for msg in msgs:
             if isinstance(msg.content, str):
                 continue
@@ -49,7 +49,7 @@ def _monkey_patch(func):
                             block["source"]["url"] = url.removeprefix(
                                 "file://",
                             )
-        return await func(msgs, **kwargs)
+        return await func(self, msgs, **kwargs)
 
     return wrapper
 
